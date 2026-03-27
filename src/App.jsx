36f0bbc6be1585c3e825cff9185a45cdc3ab43ab
SMJin <sstaw@naver.com>
2026-03-27 21:10:42 +0900

    add calculate & show results

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,31 @@
+import { useState } from "react";
 import Header from "./components/Header"
 import UserInput from "./components/UserInput"
+import Results from "./components/Results";
+
 
 function App() {
+  const [annualData, setAnnualData] = useState({
+      initialInvestment: 15000,
+      annualInvestment: 900,
+      expectedReturn: 5.5,
+      duration: 12,
+  });
+
+  const handleInputNumber = (inputIdentifier, newValue) => {
+      setAnnualData(prev => {
+          return {
+              ...prev,
+              [inputIdentifier]: +newValue,
+          }
+      })
+  }
+
   return (
     <main>
       <Header />
-      <UserInput />
+      <UserInput onChangeNumbers={handleInputNumber} />
+      <Results annualData={annualData} />
     </main>
   )
 }
--- src/components/UserInput.jsx
+++ src/components/UserInput.jsx
@@ -1,51 +1,30 @@
-import { useState } from "react"
-import { calculateInvestmentResults } from "../util/investment";
 import Input from "./Input";
 
-export default function UserInput() {
-    const [annualData, setAnnualData] = useState({
-        initialInvestment: 15000,
-        annualInvestment: 900,
-        expectedReturn: 5.5,
-        duration: 12,
-    });
-
-    let results = calculateInvestmentResults({...annualData});
-    console.log(results);
-
-    const handleInputNumber = (inputIdentifier, newValue) => {
-        setAnnualData(prev => {
-            return {
-                ...prev,
-                [inputIdentifier]: newValue,
-            }
-        })
-    }
-
-    return (
+export default function UserInput({ onChangeNumbers }) {
+        return (
         <section id="user-input">
             <div className="input-group">
                 <Input 
                     keyData={`initialInvestment`}
                     label={`Initial Investment`}
-                    onChange={handleInputNumber}
+                    onChange={onChangeNumbers}
                 />
                 <Input 
                     keyData={`annualInvestment`}
                     label={`Annual Investment`}
-                    onChange={handleInputNumber}
+                    onChange={onChangeNumbers}
                 />
             </div>
             <div className="input-group">
                 <Input 
                     keyData={`expectedReturn`}
                     label={`Expected Return`}
-                    onChange={handleInputNumber}
+                    onChange={onChangeNumbers}
                 />
                 <Input 
                     keyData={`duration`}
                     label={`Duration`}
-                    onChange={handleInputNumber}
+                    onChange={onChangeNumbers}
                 />
             </div>
         </section>
